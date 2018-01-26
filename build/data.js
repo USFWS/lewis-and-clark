bc83dd4ee9011806e016aa8d8c4de706d444904e
Roy Hewitt <roy_hewitt@fws.gov>
2018-01-26 09:21:15 -0500

    Run in watch mode unless NODE.ENV === production

--- build/data.js
+++ build/data.js
@@ -4,17 +4,27 @@ const path = require('path');
 const chokidar = require('chokidar');
 const jsonminify = require('jsonminify');
 
+const dataDir = 'src/data';
+
 function minifyJSON(file) {
-  fs.readFile(file, 'utf8', (err, json) => {
+  const basename = path.basename(file);
+  fs.readFile(`src/data/${basename}`, 'utf8', (err, json) => {
     if (err) console.error(err);
-    const filename = path.basename(file).replace('json', 'js');
+    const filename = basename.replace('json', 'js');
     writeFile(jsonminify(json), filename);
   });
 }
 
 function writeFile(json, filename) {
   const outFile = `dist/data/${filename}`;
-  fs.writeFile(outFile, json, console.log);
+  fs.writeFile(outFile, json, e => {
+    if (e) console.log(e);
+  });
 }
 
-chokidar.watch('src/data').on('change', minifyJSON);
+if (process.env.NODE_ENV === 'development')
+  chokidar.watch(dataDir).on('change', minifyJSON);
+else {
+  const files = fs.readdirSync(dataDir);
+  files.forEach(minifyJSON);
+}
